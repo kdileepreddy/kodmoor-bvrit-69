11d2d7d0603f3ff10b9df73740d9a03fd55753b5
kdileepreddy <31845473+kdileepreddy@users.noreply.github.com>
2017-09-12 15:00:30 +0530

    third

--- StudentGroup.java
+++ StudentGroup.java
@@ -1,22 +1,23 @@
 import java.util.Date;
 
 /**
- * A fix-sized array of students
- * array length should always be equal to the number of stored elements
- * after the element was removed the size of the array should be equal to the number of stored elements
- * after the element was added the size of the array should be equal to the number of stored elements
- * null elements are not allowed to be stored in the array
+ * A fix-sized array of students array length should always be equal to the
+ * number of stored elements after the element was removed the size of the array
+ * should be equal to the number of stored elements after the element was added
+ * the size of the array should be equal to the number of stored elements null
+ * elements are not allowed to be stored in the array
  * 
- * You may add new methods, fields to this class, but DO NOT RENAME any given class, interface or method
- * DO NOT PUT any classes into packages
+ * You may add new methods, fields to this class, but DO NOT RENAME any given
+ * class, interface or method DO NOT PUT any classes into packages
  *
  */
 public class StudentGroup implements StudentArrayOperation {
 
 	private Student[] students;
-	
+
 	/**
 	 * DO NOT remove or change this constructor, it will be used during task check
+	 * 
 	 * @param length
 	 */
 	public StudentGroup(int length) {
@@ -32,18 +33,31 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void setStudents(Student[] students) {
 		// Add your implementation here
+		try {
+			if (students == null) {
+				throw new IllegalArgumentException();
+			}
+		} catch (Exception ex) {
+			System.out.println(ex);
+			ex.printStackTrace();
+		}
 	}
 
 	@Override
 	public Student getStudent(int index) {
 		// Add your implementation here
-		return null;
+		if(index<0 || index >= students.length)
+		{
+			throw new IllegalArgumentException();
+		} else {
+		return students[index];}
 	}
 
 	@Override
 	public void setStudent(Student student, int index) {
 		// Add your implementation here
-	}
+		
+		}
 
 	@Override
 	public void addFirst(Student student) {
@@ -93,6 +107,7 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void bubbleSort() {
 		// Add your implementation here
+
 	}
 
 	@Override
